8e396391f72efba722f91830809798cb88550586
zhangshiqiu <zhangshiqiu1993@126.com>
2017-07-11 00:36:13 -0700

    popular movies

---
+++ spark/popular-movies.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf, SparkContext
+
+def loadMovieNames():
+    movieNames = {}
+    with open("ml-100k/u.ITEM", encoding="ISO-8859-1") as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
+
+conf = SparkConf().setMaster("local").setAppName("PopularMovies")
+sc = SparkContext(conf = conf)
+nameDict = sc.broadcast(loadMovieNames())
+
+lines = sc.textFile("file:///Users/zhangshiqiu/workspace/github/data_managing/spark/ml-100k/u.data")
+movies = lines.map(lambda x : (int(x.split()[1]), 1) )
+movieCounts = movies.reduceByKey(lambda x, y: x + y)
+
+flipped = movieCounts.map(lambda x : (x[1], x[0]))
+sortedMovies = flipped.sortByKey()
+sortedMovies = sortedMovies.map(lambda movie_count : (nameDict.value[movie_count[1]], movie_count[0]))
+
+results = sortedMovies.collect()
+for result in results:
+    print(result)
