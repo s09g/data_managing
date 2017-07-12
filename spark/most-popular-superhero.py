29f7d0dac1b871e096b6cb024e9bd03f767494f8
zhangshiqiu <zhangshiqiu1993@126.com>
2017-07-11 21:06:43 -0700

    most popular

---
+++ spark/most-popular-superhero.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+
+def parseNames(line):
+    fields = line.split('\"')
+    return (int(fields[0]), fields[1].encode('utf8'))
+
+def countCoOccurences(line):
+    elements = line.split()
+    return (int(elements[0]), len(elements) - 1)
+
+conf = SparkConf().setMaster("local").setAppName("PopularHero")
+sc = SparkContext(conf = conf)
+
+names = sc.textFile("file:///Users/zhangshiqiu/workspace/github/data_managing/spark/marvel-names.txt")
+namesRDD = names.map(parseNames)
+
+lines = sc.textFile("file:///Users/zhangshiqiu/workspace/github/data_managing/spark/marvel-graph.txt")
+pairings = lines.map(countCoOccurences)
+
+totalFriendsByCharacter = pairings.reduceByKey(lambda x, y: x + y)
+flipped = totalFriendsByCharacter.map(lambda xy : (xy[1], xy[0]))
+
+mostPopular = flipped.max()
+
+mostPopularName = namesRDD.lookup(mostPopular[1])[0]
+
+print(str(mostPopularName) + " is the most popular superhero, with " + \
+    str(mostPopular[0]) + " co-appearances.")
